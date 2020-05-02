d75323bcb79c346aab4fe26a99d61ae592c23209
Atharva Kalsekar <kalsekar.atharva7@gmail.com>
2020-05-02 19:24:12 +0530

    firebase added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyAx9tz7BKobtcJde_curXcrZrSq2bKlV3M",
+    authDomain: "shop1-4d845.firebaseapp.com",
+    databaseURL: "https://shop1-4d845.firebaseio.com",
+    projectId: "shop1-4d845",
+    storageBucket: "shop1-4d845.appspot.com",
+    messagingSenderId: "700190434824",
+    appId: "1:700190434824:web:a9ef38f1320921ba1493bd",
+    measurementId: "G-SMQDTWS5SV"
+  }
 };
 
 /*
